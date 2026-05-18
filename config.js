07e7c3b0186eecfa96c51b55fa9148fa96dde3a5
BinMaster Tech Support <bmtchspprt@gmail.com>
2026-05-18 14:38:48 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL = 'https://guszjjlvtupfdbrkpmue.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1c3pqamx2dHVwZmRicmtwbXVlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg3ODQ3NzAsImV4cCI6MjA5NDM2MDc3MH0.I4smC18vIZbMtpgAT4cAeO1WAfTZGYfop_ecvYPJ9eg';
